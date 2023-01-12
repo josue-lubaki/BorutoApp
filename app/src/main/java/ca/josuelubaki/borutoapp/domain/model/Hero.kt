c559f0eddb00657a36741e40d35fedded196ded9
Josue Lubaki <josuelubaki@gmail.com>
2023-01-12 02:37:08 -0500

    Created Hero Entity

---
+++ app/src/main/java/ca/josuelubaki/borutoapp/domain/model/Hero.kt
@@ -0,0 +1,21 @@
+package ca.josuelubaki.borutoapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ca.josuelubaki.borutoapp.util.Constants.HEROES_DATABASE_TABLE
+
+@Entity(tableName = HEROES_DATABASE_TABLE)
+data class Hero(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val image: String,
+    val about: String,
+    val rating: Double,
+    val power: Int,
+    val month: String,
+    val day: String,
+    val family: List<String>,
+    val abilities: List<String>,
+    val natureTypes: List<String>,
+)
--- app/src/main/java/ca/josuelubaki/borutoapp/util/Constants.kt
+++ app/src/main/java/ca/josuelubaki/borutoapp/util/Constants.kt
@@ -2,4 +2,5 @@ package ca.josuelubaki.borutoapp.util
 
 object Constants {
     const val DETAILS_ARGUMENT_KEY = "heroId"
+    const val HEROES_DATABASE_TABLE = "heroes_table"
 }
