2901b6f6fbee5dd76ded64ed0671f2dc48504231
Josue Lubaki <josuelubaki@gmail.com>
2023-01-14 02:48:40 -0500

    TopAppBar design

--- app/src/main/java/ca/josuelubaki/borutoapp/ui/theme/Color.kt
+++ app/src/main/java/ca/josuelubaki/borutoapp/ui/theme/Color.kt
@@ -35,3 +35,11 @@ val Colors.inactiveIndicatorColor : Color
 @get:Composable
 val Colors.buttonBackgroundColor : Color
     get() = if (isLight) Purple500 else Purple700
+
+@get:Composable
+val Colors.topAppBarContentColor : Color
+    get() = if (isLight) Color.White else LightGray
+
+@get:Composable
+val Colors.topAppBarBackgroundColor : Color
+    get() = if (isLight) Purple500 else Color.Black
