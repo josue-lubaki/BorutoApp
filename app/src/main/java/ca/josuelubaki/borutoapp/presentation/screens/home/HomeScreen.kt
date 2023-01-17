be01732010941e65907b7b3ab22b6e0e5431e552
Josue Lubaki <70981701+josue-lubaki@users.noreply.github.com>
2023-01-16 23:55:33 -0500

    Created getAllHeroes Use Case

--- app/src/main/java/ca/josuelubaki/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/ca/josuelubaki/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -3,7 +3,6 @@ package ca.josuelubaki.borutoapp.presentation.screens.home
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
