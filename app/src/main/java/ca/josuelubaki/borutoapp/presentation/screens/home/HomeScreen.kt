08ca92be225768c9545bd8d0a19ed91be296b13c
Josue Lubaki <josuelubaki@gmail.com>
2023-01-17 02:08:20 -0500

    Design RatingWidget - calculation

--- app/src/main/java/ca/josuelubaki/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/ca/josuelubaki/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,9 +1,16 @@
 package ca.josuelubaki.borutoapp.presentation.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import ca.josuelubaki.borutoapp.presentation.components.RatingWidget
+import ca.josuelubaki.borutoapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
@@ -17,6 +24,11 @@ fun HomeScreen(
             HomeTopBar(onSearchClicked = {})
         },
     ){
-        it
+        Box(modifier = Modifier.padding(it)){
+            RatingWidget(
+                modifier = Modifier.padding(LARGE_PADDING),
+                rating = 3.7
+            )
+        }
     }
 }
