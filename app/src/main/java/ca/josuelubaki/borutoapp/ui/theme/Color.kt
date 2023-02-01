18290a45b176cfe892649e37662d96538cc9f184
Josue Lubaki <josuelubaki@gmail.com>
2023-02-01 01:12:50 -0500

    ShimmerEffect animation

--- app/src/main/java/ca/josuelubaki/borutoapp/ui/theme/Color.kt
+++ app/src/main/java/ca/josuelubaki/borutoapp/ui/theme/Color.kt
@@ -13,6 +13,11 @@ val LightGray = Color(0xFFD8D8D8)
 val DarkGray = Color(0xFF2A2A2A)
 val StarColor = Color(0xFFFFC94D)
 
+// shimmer effect colors
+val ShimmerLightGray = Color(0xFFF1F1F1)
+val ShimmerMediumGray = Color(0xFFE3E3E3)
+val ShimmerDarkGray = Color(0xFF1D1D1D)
+
 @get:Composable
 val Colors.welcomeScreenBackgroundColor : Color
     get() = if (isLight) Color.White else Color.Black
