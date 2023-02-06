79d3ced1e11a624f401b1779a7a37309fe164af8
Josue Lubaki <josuelubaki@gmail.com>
2023-02-06 01:18:56 -0500

    added Paging Source Unit Tests

---
+++ app/src/test/java/ca/josuelubaki/borutoapp/data/remote/FakeBorutoApi.kt
@@ -0,0 +1,122 @@
+package ca.josuelubaki.borutoapp.data.remote
+
+import ca.josuelubaki.borutoapp.domain.model.ApiResponse
+import ca.josuelubaki.borutoapp.domain.model.Hero
+
+class FakeBorutoApi : BorutoApi {
+
+    private val heroes =  listOf(
+        Hero(
+            id = 1,
+            name = "Sasuke",
+            image = "/images/sasuke.jpg",
+            about = "Sasuke Uchiha (うちはサスケ, Uchiha Sasuke) is one of the last surviving members of Konohagakure's Uchiha clan. After his older brother, Itachi, slaughtered their clan, Sasuke made it his mission in life to avenge them by killing Itachi. He is added to Team 7 upon becoming a ninja and, through competition with his rival and best friend, Naruto Uzumaki.",
+            rating = 5.0,
+            power = 98,
+            month = "July",
+            day = "23rd",
+            family = listOf(
+                "Fugaku",
+                "Mikoto",
+                "Itachi",
+                "Sarada",
+                "Sakura"
+            ),
+            abilities = listOf(
+                "Sharingan",
+                "Rinnegan",
+                "Sussano",
+                "Amateratsu",
+                "Intelligence"
+            ),
+            natureTypes = listOf(
+                "Lightning",
+                "Fire",
+                "Wind",
+                "Earth",
+                "Water"
+            )
+        ),
+        Hero(
+            id = 2,
+            name = "Naruto",
+            image = "/images/naruto.jpg",
+            about = "Naruto Uzumaki (うずまきナルト, Uzumaki Naruto) is a shinobi of Konohagakure's Uzumaki clan. He became the jinchūriki of the Nine-Tails on the day of his birth — a fate that caused him to be shunned by most of Konoha throughout his childhood. After joining Team Kakashi, Naruto worked hard to gain the village's acknowledgement all the while chasing his dream to become Hokage.",
+            rating = 5.0,
+            power = 98,
+            month = "Oct",
+            day = "10th",
+            family = listOf(
+                "Minato",
+                "Kushina",
+                "Boruto",
+                "Himawari",
+                "Hinata"
+            ),
+            abilities = listOf(
+                "Rasengan",
+                "Rasen-Shuriken",
+                "Shadow Clone",
+                "Senin Mode"
+            ),
+            natureTypes = listOf(
+                "Wind",
+                "Earth",
+                "Lava",
+                "Fire"
+            )
+        ),
+        Hero(
+            id = 3,
+            name = "Sakura",
+            image = "/images/sakura.jpg",
+            about = "Sakura Uchiha (うちはサクラ, Uchiha Sakura, née Haruno (春野)) is a kunoichi of Konohagakure. When assigned to Team 7, Sakura quickly finds herself ill-prepared for the duties of a shinobi. However, after training under the Sannin Tsunade, she overcomes this, and becomes recognised as one of the greatest medical-nin in the world.",
+            rating = 4.5,
+            power = 92,
+            month = "Mar",
+            day = "28th",
+            family = listOf(
+                "Kizashi",
+                "Mebuki",
+                "Sarada",
+                "Sasuke"
+            ),
+            abilities = listOf(
+                "Chakra Control",
+                "Medical Ninjutsu",
+                "Strength",
+                "Intelligence"
+            ),
+            natureTypes = listOf(
+                "Earth",
+                "Water",
+                "Fire"
+            )
+        )
+    )
+
+    override suspend fun getAllHeroes(page: Int): ApiResponse {
+        return ApiResponse(
+            success = false,
+        )
+    }
+
+    override suspend fun searchHeroes(name: String): ApiResponse {
+        val searchHeroes = findHeroesByName(name)
+        return ApiResponse(
+            success = true,
+            message = "OK",
+            heroes = searchHeroes
+        )
+    }
+
+    private fun findHeroesByName(name: String) : List<Hero> {
+        return if(name.isNotEmpty()){
+            heroes.filter { hero ->
+                hero.name.contains(name, true)
+            }
+        } else {
+            emptyList()
+        }
+    }
+}
