528433c801ee888e62b209f0926642304140f1a7
Josue Lubaki <josuelubaki@gmail.com>
2023-02-07 15:45:32 -0500

    added RemoteMediator unit tests

--- app/src/main/java/ca/josuelubaki/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/ca/josuelubaki/borutoapp/data/local/BorutoDatabase.kt
@@ -1,6 +1,8 @@
 package ca.josuelubaki.borutoapp.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import ca.josuelubaki.borutoapp.data.local.dao.HeroDao
@@ -11,6 +13,21 @@ import ca.josuelubaki.borutoapp.domain.model.HeroRemoteKeys
 @Database(entities = [Hero::class, HeroRemoteKeys::class], version = 1, exportSchema = false)
 @TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase : RoomDatabase() {
+
+    companion object {
+        fun create(context : Context, useInMemory: Boolean) : BorutoDatabase {
+            val databaseBuilder = if(useInMemory) {
+                Room.inMemoryDatabaseBuilder(context, BorutoDatabase::class.java)
+            } else {
+                Room.databaseBuilder(context, BorutoDatabase::class.java, "test_database.db")
+            }
+
+            return databaseBuilder
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeysDao(): HeroRemoteKeysDao
 }
